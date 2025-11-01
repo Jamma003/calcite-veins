0db618e1023db3586a1ae315b6625931d9e2011d
Jamma003 <jamma003@gmail.com>
2025-11-01 13:26:52 +0000

    initial commit

---
+++ calcite-veins-1.20.1-master/README.md
@@ -0,0 +1,1 @@
+Calcite Veins adds large calcite-based ore veins across various depths, to encourage chunk mining and cave exploration
---
+++ calcite-veins-1.20.1-master/src/main/java/net/jamma003/calciteveins/CalciteVeinsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.jamma003.calciteveins;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CalciteVeinsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
